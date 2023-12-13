7da55c6cceeecb23222f7e52fd55ae5081263b1d
dillonzellis <dillonzellis@gmail.com>
2023-12-13 14:21:17 -0500

    make landing page left aligned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="mx-auto flex flex-col gap-y-6 justify-center text-center max-w-3xl items-center min-h-screen px-20">
+    <div className="mx-auto flex flex-col gap-y-6 justify-center max-w-3xl items-center- min-h-screen px-8">
       <h1 className="text-4xl font-bold text-gray-900 sm:text-6xl">
         Welcome to <span className="text-blue-600">Walking After Midnight</span>
       </h1>
       <p className="text-gray-700 text-2xl">Enjoy your stay!</p>
-      <Link href="/" className={buttonVariants()}>
+      <Link href="/" className={buttonVariants({ className: "self-start" })}>
         Sign In
       </Link>
     </div>
