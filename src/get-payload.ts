fbe0ae99b3fb978102eab2da29bf91a108a40b2b
dillonzellis <dillonzellis@gmail.com>
2023-12-13 15:58:47 -0500

    connect mongodb and modify dev script to use express server

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
-import payload from "payload";
+import payload, { Payload } from "payload";
 import type { InitOptions } from "payload/config";
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
