35dde7fa83f59ebb8f2e0ebde65a02ad6c776f7c
dillonzellis <dillonzellis@gmail.com>
2023-12-13 13:38:00 -0500

    basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Yeppers</div>;
+  return (
+    <div className="mx-auto flex flex-col gap-y-6 justify-center text-center max-w-3xl items-center min-h-screen px-20">
+      <h1 className="text-4xl font-bold text-gray-900 sm:text-6xl">
+        Welcome to <span className="text-blue-600">Walking After Midnight</span>
+      </h1>
+      <p className="text-gray-700 text-2xl">Enjoy your stay!</p>
+      <Link href="/" className={buttonVariants()}>
+        Sign In
+      </Link>
+    </div>
+  );
 }
