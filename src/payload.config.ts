0b633eacc840c2a93d4ad3a2a63fd1e244f4ea16
dillonzellis <dillonzellis@gmail.com>
2023-12-14 12:42:04 -0500

    add auth-router to to payload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,15 +2,22 @@ import { buildConfig } from "payload/config";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
-import path from "path";
+import path, { resolve } from "path";
+import { Users } from "./collections/Users";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [],
+  collections: [Users],
   routes: {
     admin: "/dashboard",
   },
   admin: {
+    user: "users",
     bundler: webpackBundler(),
     meta: {
       titleSuffix: "- Walking After Midnight",
