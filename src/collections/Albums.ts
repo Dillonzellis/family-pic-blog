a6e1058f38e368242465ccd71efbbf437fb2b8a6
dillonzellis <dillonzellis@gmail.com>
2023-12-19 18:26:27 -0500

    add Albums collection

---
+++ src/collections/Albums.ts
@@ -0,0 +1,56 @@
+import { Access, CollectionConfig } from "payload/types";
+
+export const Albums: CollectionConfig = {
+  slug: "albums",
+  admin: {
+    useAsTitle: "albums",
+  },
+  access: {
+    read: () => true,
+    create: () => true,
+    update: () => true,
+    delete: () => true,
+  },
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
+    },
+    {
+      name: "title",
+      label: "Title",
+      type: "text",
+    },
+    {
+      name: "description",
+      type: "textarea",
+      label: "Caption",
+    },
+    {
+      name: "images",
+      type: "array",
+      label: "Product images",
+      minRows: 1,
+      maxRows: 4,
+      required: true,
+      labels: {
+        singular: "Image",
+        plural: "Images",
+      },
+      fields: [
+        {
+          name: "image",
+          type: "upload",
+          relationTo: "media",
+          required: true,
+        },
+      ],
+    },
+  ],
+};
