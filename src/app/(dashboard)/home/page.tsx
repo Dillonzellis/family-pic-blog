fbd22da6659641de14cabd8dbb19c28b62344a43
dillonzellis <dillonzellis@gmail.com>
2023-12-21 14:50:01 -0500

    add dashboard layout route

---
+++ src/app/(dashboard)/home/page.tsx
@@ -0,0 +1,16 @@
+import AlbumReel from "@/components/AlbumReel";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+const HomePage = () => {
+  return (
+    <MaxWidthWrapper>
+      <div className="pt-20">
+        <div>Uploads</div>
+        <div className="grid grid-cols-4"></div>
+        <AlbumReel query={{ sort: "desc" }} title="All Albums" />
+      </div>
+    </MaxWidthWrapper>
+  );
+};
+
+export default HomePage;
