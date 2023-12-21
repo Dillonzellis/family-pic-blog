e004141ca5cb9a56ed34dbc67280044fbef821c9
dillonzellis <dillonzellis@gmail.com>
2023-12-21 14:17:37 -0500

    add link to album page from profile page

--- src/app/profile/[profileId]/page.tsx
+++ src/app/profile/[profileId]/page.tsx
@@ -3,9 +3,8 @@ import { buttonVariants } from "@/components/ui/button";
 import { getPayloadClient } from "@/get-payload";
 import { getServerSideUser } from "@/lib/payload-utils";
 import { cookies } from "next/headers";
+import Image from "next/image";
 import Link from "next/link";
-
-// TODO: add gaurd check for if no albums
 
 interface ProfilePageProps {
   params: {
@@ -30,44 +29,37 @@ const ProfilePage = async ({ params }: ProfilePageProps) => {
     },
   });
 
-  // console.log(albums);
-
-  albums.forEach((album) => {
-    console.log(album.thumbnail.url);
-  });
-
-  const albumTitles = albums.map((album) => album.title);
-  const desc = albums.map((album) => album.description);
-
-  const createdAt = albums.map((album) => album.createdAt);
-  const thumbnail = albums.map((album) => album.thumbnail.url);
-
-  const allImagesFlattened = albums.flatMap((album) => album.images);
-
-  const imageTitles = allImagesFlattened.map((item) => item.image.title);
-
-  console.log(allImagesFlattened);
+  // If no albums, you can return a message or handle it as needed
+  // TODO: add guard clause for no albums
+  if (albums.length === 0) {
+    return <div>No albums found</div>;
+  }
 
   return (
     <MaxWidthWrapper>
       <div className="pt-20">
         <div className="pb-6">Profile Page</div>
         <div className="text-xl pb-12">Welcome {user?.name}!</div>
-        <div className="text-xl pb-12">Album Titles:</div>
-        {albumTitles.map((title, index) => (
-          <div key={index} className="pb-2">
-            {title}
-          </div>
-        ))}
-        <div>{thumbnail}</div>
-        <div>{createdAt}</div>
-        <div>{desc}</div>
-        {imageTitles.map((imgTitle, i) => (
-          <div key={i}>Image Titles {imgTitle}</div>
-        ))}
+
         <Link href="/dashboard" className={buttonVariants()}>
           Upload Media
         </Link>
+
+        {albums.map((album, index) => (
+          <Link href={`/album/${album.id}`} key={index}>
+            <div>
+              <h2 className="text-2xl">{album.title}</h2>
+              <Image
+                src={album.thumbnail.url}
+                alt={`Thumbnail for ${album.title}`}
+                height={500}
+                width={500}
+              />
+              <p>Description: {album.description}</p>
+              <p>Created At: {album.createdAt}</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </MaxWidthWrapper>
   );
