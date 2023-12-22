96fd4925aa92c49b7d0392bf245705731b02db3a
dillonzellis <dillonzellis@gmail.com>
2023-12-22 17:26:32 -0500

    desktop view of all albums

--- src/app/(dashboard)/all-albums/page.tsx
+++ src/app/(dashboard)/all-albums/page.tsx
@@ -1,13 +1,52 @@
-import AlbumReel from "@/components/AlbumReel";
+import Link from "next/link";
+import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { getPayloadClient } from "@/get-payload";
 
-const AllAlbumsPage = () => {
+const AllAlbumsPage = async () => {
+  const payload = await getPayloadClient();
+
+  const { docs: albums } = await payload.find({
+    collection: "albums",
+  });
+
+  if (albums.length === 0) {
+    return (
+      <MaxWidthWrapper>
+        <div>No albums found</div>
+      </MaxWidthWrapper>
+    );
+  }
+
   return (
     <MaxWidthWrapper>
-      <div className="pt-20">
-        <div>Uploads</div>
-        <div className="grid grid-cols-4"></div>
-        <AlbumReel query={{ sort: "desc" }} title="All Albums" />
+      <div className="pt-16">
+        <div className="pb-16 text-5xl font-semibold">All Albums</div>
+        <div className="flex flex-wrap gap-10">
+          {albums.map((album, index) => (
+            <div key={index}>
+              <Link href={`/album/${album.id}`}>
+                <Image
+                  src={album.thumbnail.url}
+                  alt={`Thumbnail for ${album.title}`}
+                  height={279}
+                  width={325}
+                  className="h-[279px] w-[325px] rounded-lg object-cover"
+                />
+              </Link>
+              <Link
+                href={`/profile/${album.user.id}`}
+                className="inline-block py-1.5 text-base font-medium"
+              >
+                {album.user.name}
+              </Link>
+              <p className="pb-0.5 text-base">{album.title}</p>
+              <p className="text-sm text-muted-foreground">
+                {album.description}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </MaxWidthWrapper>
   );
