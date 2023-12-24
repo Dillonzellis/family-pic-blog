54517578a50622a6f65d4e0d8df1dc8f44c6ed8f
dillonzellis <dillonzellis@gmail.com>
2023-12-24 11:53:37 -0500

    change backto home btn label

--- src/components/BackToRootBtn.tsx
+++ src/components/BackToRootBtn.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const BackToRootButton: React.FC = () => {
   return (
     <a href="/" className="rounded bg-blue-500 px-4 py-2 text-white">
-      Back to Site
+      Home
     </a>
   );
 };
